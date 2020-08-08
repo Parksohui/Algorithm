f121325be8997b85155cf56c1f8e72722a3f77b5
Parksohui <psh990525@naver.com>
2020-08-08 15:06:19 +0900

    백준 10821 정수의 개수

---
+++ 백준10821Num.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class 백준10821Num {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		
+		String a=input.next();
+		
+		String[] array=a.split(",");
+		
+		System.out.println(array.length);
+
+	}
+}
