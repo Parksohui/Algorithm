f24449e05c0348adb6ad1b8ddb967b580afce7c0
Parksohui <psh990525@naver.com>
2020-12-21 23:10:22 +0900

    x

--- programmers/K번째수.py
+++
@@ -1,8 +0,0 @@
-def solution(array, commands):
-    arraytemp=[]
-    answer=[]
-    for i in range(len(commands)):
-        arraytemp=array[commands[i][0]-1: commands[i][1]]
-        arraytemp.sort()
-        answer.append(arraytemp[commands[i][2]-1])
-    return answer
--- programmers/완주하지 못한 선수.py
+++
@@ -1,6 +0,0 @@
-def solution(participant, completion):
-    for i in completion:
-        if i in participant:
-            participant.remove(i)
-
-    return participant[0]
