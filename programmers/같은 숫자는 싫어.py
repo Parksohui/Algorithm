db33059071bc826fa68a9b636146522d2f9036dd
Parksohui <psh990525@naver.com>
2020-12-22 23:26:50 +0900

    programmers 같은 숫자는 싫어

--- programmers/같은 숫자는 싫어.py
+++ programmers/같은 숫자는 싫어.py
@@ -3,7 +3,6 @@ def solution(arr):
     for i in range(len(arr) - 1):
         if (arr[i] != arr[i + 1]):
             answer.append(arr[i])
-    if (arr[-1] != answer[-1]):
-        answer.append(arr[-1])
+    answer.append(arr[-1])
 
     return answer
