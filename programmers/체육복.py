969d3285a7e793740a9b2aa776239d2ad2034485
Parksohui <psh990525@naver.com>
2021-01-04 20:44:33 +0900

    programmers 체육복

--- programmers/체육복.py
+++ programmers/체육복.py
@@ -4,13 +4,13 @@ def solution(n, lost, reserve):
     for i in range(n):
         temp.append(1)
     for i in lost:
-        temp[i - 1] = 0
+        temp[i - 1] -= 1
     for i in reserve:
         temp[i - 1] += 1
 
     for i in range(len(temp)):
         if (temp[i] == 0):
-            if (i != 0 or i != len(temp) - 1):
+            if (i != 0 and i < len(temp) - 1):
                 if (temp[i - 1] == 2):
                     temp[i - 1] = 1
                     temp[i] = 1
