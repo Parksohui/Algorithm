8b6935620f9164923ea0f9243544c72de7f47236
Parksohui <psh990525@naver.com>
2021-01-25 15:31:22 +0900

    programmers 신규 아이디 추천
    61.5/100
    - 어디 잘못된 것인지 알 수 없음
    -아직 해결 불가

---
+++ programmers/신규 아이디 추천.py
@@ -0,0 +1,45 @@
+def solution(new_id):
+    answer = ''
+
+    new_id = new_id.lower()  # 1단계
+
+    new_id = list(new_id)  # list로 변환
+
+    i = 0
+    while (i < len(new_id)):  # 2단계
+        x = new_id[i]
+        if (x.isalpha() or x.isdigit() or x == '-' or x == '_' or x == '.'):
+            i += 1
+            pass
+        else:
+            del new_id[i]
+
+    i = 0
+    while (i < len(new_id) - 1):  # 3단계
+        if (new_id[i] == new_id[i + 1]):
+            del new_id[i + 1]
+        else:
+            i += 1
+
+    # 4단계
+    if (new_id[0] == '.'):
+        del new_id[0]
+    elif (new_id[-1] == '.'):
+        del new_id[-1]
+
+    # 5단계
+    if (len(new_id) == 0):
+        new_id.append("a")
+
+    # 6단계
+    if (len(new_id) >= 16):
+        new_id = new_id[:15]
+        if (new_id[-1] == '.'):
+            del new_id[-1]
+
+    # 7단계
+    while (len(new_id) <= 2):
+        new_id.append(new_id[-1])
+
+    answer = ''.join(new_id)
+    return answer
