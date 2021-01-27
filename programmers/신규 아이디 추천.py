77bb573079ffabd43f06fd0a44650fdc5b3702a6
Parksohui <psh990525@naver.com>
2021-01-28 00:03:12 +0900

    programmers 신규 아이디 추천

--- programmers/신규 아이디 추천.py
+++ programmers/신규 아이디 추천.py
@@ -15,17 +15,20 @@ def solution(new_id):
             del new_id[i]
 
     i = 0
-    while (i < len(new_id) - 1):  # 3단계
-        if (new_id[i] == new_id[i + 1]):
-            del new_id[i + 1]
-        else:
-            i += 1
+    if (len(new_id) != 0):
+        while (i < len(new_id) - 1):  # 3단계
+            if (new_id[i] == "." and new_id[i] == new_id[i + 1]):
+                del new_id[i + 1]
+            else:
+                i += 1
 
     # 4단계
-    if (new_id[0] == '.'):
-        del new_id[0]
-    elif (new_id[-1] == '.'):
-        del new_id[-1]
+    if (len(new_id) != 0):
+        if (new_id[0] == '.'):
+            del new_id[0]
+    if (len(new_id) != 0):
+        if (new_id[-1] == '.'):
+            del new_id[-1]
 
     # 5단계
     if (len(new_id) == 0):
