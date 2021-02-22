c8c1b915069d584b69051c300c2c50b5d254facb
Parksohui <psh990525@naver.com>
2021-02-23 00:19:04 +0900

    5988 홀수일까 짝수일까

---
+++ Baekjoon/백준5988홀수짝수.py
@@ -0,0 +1,10 @@
+if __name__=='__main__' :
+    x=int(input())
+
+    for i in range(x):
+        temp=int(input())
+
+        if temp%2==0:
+            print("even")
+        else:
+            print("odd")
