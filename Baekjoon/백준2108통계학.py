b1d5ae1461d94159723be48e12419342eee60185
Parksohui <psh990525@naver.com>
2021-02-23 00:40:07 +0900

    2108 통계학
    - 시간 초과 문제 해결
      -> 1) input입력 -> sys 입력
          2) 최빈값 구하는 방법
    - 최빈값을 구할 때 collections에서 제공하는 Counter와 most_common을 사용하면 된다는 것을 알게됨

--- Baekjoon/백준2108통계학.py
+++ Baekjoon/백준2108통계학.py
@@ -1,9 +1,12 @@
+import sys
+from collections import  Counter
+
 if __name__=='__main__' :
-    x=int(input())
+    x=int(sys.stdin.readline().strip())
 
     result=[]
     for i in range(x):
-        result.append(int(input()))
+        result.append(int(sys.stdin.readline().strip()))
 
     print(round(sum(result)/x)) #산술평균
 
@@ -11,23 +14,18 @@ if __name__=='__main__' :
 
     print(result[x//2]) #중앙값
 
-    maxnumber=[result[0]]
-    maxcount=result.count(result[0])
+    # 데이터 개수가 많은 순으로 정렬된 배열을 반환
+    mostvalue=Counter(result).most_common()
+    value=0 #최빈값
 
-    for i in range(1,len(result)):
-        count=result.count(result[i])
-
-        if maxcount < count:
-            maxnumber=[result[i]]
-            maxcount=count
-        elif maxcount==count:
-            maxnumber.append(result[i])
-
-    maxnumber=list(set(maxnumber))
-    if len(maxnumber)!=1:
-        maxnumber.sort()
-        print(maxnumber[1])
+    if len(mostvalue)>1: #list 길이가 1보다 클때
+        if mostvalue[0][1]==mostvalue[1][1]: #최빈값이 여러개라면
+            value=mostvalue[1][0] #두 번째로 작은 값 출력
+        else: #최빈값이 한개라면
+            value=mostvalue[0][0]
     else:
-        print(maxnumber[0]) #최빈값
+        value=mostvalue[0][0]
+
+    print(value) #최빈값 출력
 
     print(max(result)-min(result)) #범위
