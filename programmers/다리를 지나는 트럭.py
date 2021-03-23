83e257a70094cbd25d03fe9a573435966df5e81b
Parksohui <psh990525@naver.com>
2021-03-23 21:53:13 +0900

    programmers 다리를 지나는 트럭
    - 처음과 다른 방향으로 접근
    - 다리 길이만큼 리스트를 선언한 후 접근하는 방식
    - 잘 풀리지 않아 검색 통하여 참고..

--- programmers/다리를 지나는 트럭.py
+++ programmers/다리를 지나는 트럭.py
@@ -1,28 +1,19 @@
+
 def solution(bridge_length, weight, truck_weights):
-    answer = 1
 
-    temp = []
-    truck_weights.sort()
-    temp.append(truck_weights.pop())
+    answer =0
+    temp =[0 ] *bridge_length  # 다리 길이만큼 리스트 선언
 
-    while True:
-        if (len(temp) == 0):
-            answer += bridge_length - 1
 
-            if (len(truck_weights) == 0):
-                break
-            else:
-                temp.append(truck_weights.pop())
+    while(temp):
+        temp.pop(0)  # 앞으로 한칸씩 당겨짐
+        if len(truck_weights ) >0:  # 트럭이 남아있으면
+            if sum(temp ) +truck_weights[0 ]<=weight:  # 무게를 견딘다면
+                temp.append(truck_weights.pop(0))  # 다리에 올라감
+            else:  # 무게를 못견딘다면
+                temp.append(0)  # 다리에 있는 트럭 마저 지나가도록
 
-        if truck_weights:
-            if (sum(temp) + truck_weights[0] < weight):
-                temp.append(truck_weights.pop(0))
-                answer += 1
-            else:
-                temp.pop(0)
-        else:
-            temp.pop(0)
+        answer +=1  # 1초 지남
 
-    # answer+=bridge_length
 
-    return answer + 1
+    return answer
