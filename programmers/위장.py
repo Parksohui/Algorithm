9bcae09af4a00c1ee27677b2613ec2706a95984f
Parksohui <psh990525@naver.com>
2021-03-31 23:07:37 +0900

    programmers 위장
    - 해시 문제
    - 저번 코드에서 달라진 점: 최종 answer 구하는 방법
       -> 수학적으로 접근
       -> (각 갯수 +1)을 하여 곱한 후 최종 값에서 1을 빼줌
       -> 1을 더하는 이유: 안입는 경우 포함
       -> 1을 빼는 이유: 아무것도 안입는 경우

--- programmers/위장.py
+++ programmers/위장.py
@@ -1,27 +1,23 @@
 def solution(clothes):
-    answer = 0
+    answer = 1
 
     temp = [[clothes[0][1], clothes[0][0]]]
     for i in range(1, len(clothes)):
         count = 0
         for j in range(len(temp)):
-            if (temp[j][0] == clothes[i][1]):
+            if (temp[j][0] == clothes[i][1]):  # clothes 같은 종류라면
                 temp[j].append(clothes[i][0])
                 break
             else:
                 count += 1
-        if (count == len(temp)):
+        if (count == len(temp)):  # 끝까지 돌았는데 같은 종류가 없다면 추가
             temp.append([clothes[i][1], clothes[i][0]])
 
-    print(temp)
+    # (갯수+1) * (갯수 +1) -1
+    # +1을 해주는 이유는 안입는 경우를 위하여 +1을 해줌
+    # -1을 해주는 이유는 아무것도 안입는 경우를 위하여 -1을 해줌
 
-    answer += len(temp[0]) - 1
+    for i in range(len(temp)):
+        answer *= len(temp[i])
 
-    if (len(temp) != 1):
-        multi = len(temp[0]) - 1
-        for i in range(1, len(temp)):
-            answer += len(temp[i]) - 1
-            multi *= len(temp[i]) - 1
-        answer += multi
-
-    return answer
+    return answer - 1
