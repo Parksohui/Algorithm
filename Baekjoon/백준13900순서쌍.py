82d7272007423e8c5fbf95e79fd30dc54646e64a
Parksohui <psh990525@naver.com>
2021-08-15 21:04:06 +0900

    13900 순서쌍의 곱의 합

--- Baekjoon/백준13900순서쌍.py
+++ Baekjoon/백준13900순서쌍.py
@@ -2,7 +2,7 @@ import  sys
 
 # 조합, sum, 이중 for문, while 쓰는 경우 시간 초과 발생
 # ex) 2 3 4 이면 (2+3)*4 + 2 * 3 이런식으로 접근
-# 위의 방법으로 접글할때도 sum() 사용하면 시간 초과 발생
+# 위의 방법으로 접근할때도 sum() 사용하면 시간 초과 발생
 
 if __name__=='__main__' :
     N=sys.stdin.readline().strip()
