76d02f3de1f1c4cb8cceef96398724b823bdab64
Parksohui <psh990525@naver.com>
2021-09-04 20:12:44 +0900

    programmers_2018 카카오 블라인드_[3차] 파일명 정렬

--- programmers/[3차] 파일명 정렬.py
+++ programmers/[3차] 파일명 정렬.py
@@ -1,20 +1,26 @@
 def solution(files):
     answer = []
-
     temp = []
+
     for i in files:
         head, num, tail = '', '', ''
         check = False
+        numcheck = False
         for j in i:
-            if not j.isdigit() and check == False:  # HEAD
-                head += j
-            elif j.isdigit():  # NUMBER
-                num += j
-                check = True
-            else:  # TAIL
-                if check == True:
+            if not j.isdigit():
+                if check == True:  # Tail
                     tail += j
-
+                    numcheck = True
+                else:  # HEAD
+                    head += j
+            elif j.isdigit():
+                if numcheck == True:  # Tail
+                    tail += j
+                else:  # NUMBER
+                    num += j
+                    check = True
+                    if len(num) >= 5:  # NUMBER 한글자 ~ 최대 다섯 글자
+                        numcheck = True
         temp.append([head, num, tail])
 
     temp.sort(key=lambda x: (x[0].upper(), int(x[1])))  # 정렬) 1. HEAD 기준 2. NUMBER 기준 3. 입력 순서
@@ -23,3 +29,5 @@ def solution(files):
         answer.append(''.join(i))
 
     return answer
+
+# abc123defg123 의 경우 NUMBER과 TAIL 구분 확실히 해주어야 통과
